52db525b71b5144f352d30c72d31200c0157f6c1
Ivan <ivmishevski@gmail.com>
2025-02-02 23:59:38 +0200

    Added Watchlist Model

---
+++ Movie Catalog/Models/Watchlist.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Catalog.Models
+{
+    public class Watchlist
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public User? User { get; set; } 
+
+        [Required]
+        public int MovieId { get; set; }
+        public Movie? Movie { get; set; } 
+
+        public DateTime AddedAt { get; set; } = DateTime.UtcNow; 
+    }
+}
