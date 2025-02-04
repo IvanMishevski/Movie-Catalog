ca8b3cc3b730da23a372e682daf07dbb098bb809
aylin-s06 <123801066+aylin-s06@users.noreply.github.com>
2025-02-04 09:29:11 +0200

    Update Movie.cs
    Add GenreId 
    Add DirectorId
    Add MovieActors

--- Movie Catalog/Models/Movie.cs
+++ Movie Catalog/Models/Movie.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 
 namespace Movie_Catalog.Models
 {
@@ -17,8 +18,19 @@ namespace Movie_Catalog.Models
         public int ReleaseYear { get; set; } 
 
         [Required]
-        public int Duration { get; set; } 
+        public int Duration { get; set; }
 
+        [Required]
+        public int GenreId { get; set; } 
+        [ForeignKey("GenreId")]
+        public Genre? Genre { get; set; }
+
+        [Required]
+        public int DirectorId { get; set; } 
+        [ForeignKey("DirectorId")]
+        public Director? Director { get; set; }
+
+        public ICollection<MovieActor> MovieActors { get; set; } = new List<MovieActor>(); 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
     }
