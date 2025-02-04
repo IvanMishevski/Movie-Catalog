7872f86dfd557d87bdff6c8ae8a4be154addd3fa
aylin-s06 <123801066+aylin-s06@users.noreply.github.com>
2025-02-04 10:44:59 +0200

    Create Review.cs

---
+++ Movie Catalog/Models/Review.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movie_Catalog.Models
+{
+    public class Review
+    {
+        public int Id { get; set; } 
+
+        [Required]
+        public int UserId { get; set; } 
+        public User? User { get; set; }
+
+        [Required]
+        public int MovieId { get; set; } 
+        [ForeignKey("MovieId")]
+        public Movie? Movie { get; set; }
+
+        [Required]
+        [Range(0, 10)]
+        public decimal Rating { get; set; } 
+
+        public string? ReviewText { get; set; } 
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
+    }
+}
