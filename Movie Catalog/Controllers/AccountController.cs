89ecb166bb312274510f88237a142aae8ca00d35
Ivan <ivmishevski@gmail.com>
2025-02-09 22:33:27 +0200

    Added AccountController

---
+++ Movie Catalog/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Catalog.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
