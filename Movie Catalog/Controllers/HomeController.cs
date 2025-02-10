9fe36cc5638289dc603993ec0337290bb7602d8e
Ivan <ivmishevski@gmail.com>
2025-02-11 00:21:23 +0200

    Added searchbar functionallity

--- Movie Catalog/Controllers/HomeController.cs
+++ Movie Catalog/Controllers/HomeController.cs
@@ -16,23 +16,34 @@ namespace Movie_Catalog.Controllers
             _context = context;
         }
 
-        public IActionResult Index(string searchString)
+        public IActionResult Index(string searchString, string filter)
         {
             var movies = _context.Movies
-            .Include(m => m.Genre)
-            .Include(m => m.Director)
-            .AsQueryable();
+        .Include(m => m.Genre)
+        .Include(m => m.Director)
+        .AsQueryable();
 
             if (!string.IsNullOrEmpty(searchString))
             {
-                movies = movies.Where(m =>
-                    m.Title.Contains(searchString) ||
-                    m.Description.Contains(searchString) ||
-                    m.Genre.Name.Contains(searchString) ||
-                    m.Director.Name.Contains(searchString)
-                );
+                movies = filter switch
+                {
+                    "title" => movies.Where(m => m.Title.Contains(searchString)),
+                    "genre" => movies.Where(m => m.Genre.Name.Contains(searchString)),
+                    "year" => movies.Where(m => m.ReleaseYear.ToString().Contains(searchString)),
+                    _ => movies.Where(m => m.Title.Contains(searchString))
+                };
             }
 
+            movies = filter switch
+            {
+                "title" => movies.OrderBy(m => m.Title),
+                "genre" => movies.OrderBy(m => m.Genre.Name),
+                "year" => movies.OrderBy(m => m.ReleaseYear),
+                _ => movies.OrderBy(m => m.Title)
+            };
+
+            ViewData["CurrentFilter"] = filter;
+            ViewData["SearchString"] = searchString;
             return View(movies.ToList());
         }
 
