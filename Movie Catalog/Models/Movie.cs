248e87bd7deb6caca161a1967b8561ea18546a11
aylin-s06 <123801066+aylin-s06@users.noreply.github.com>
2025-02-11 16:26:35 +0200

    Update Movie.cs
    Add Statistic and Reviews to Movie Model

--- Movie Catalog/Models/Movie.cs
+++ Movie Catalog/Models/Movie.cs
@@ -1,39 +1,44 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
+using MovieCatalog.Models;
 
-namespace Movie_Catalog.Models
+namespace MovieCatalog.Models
 {
     public class Movie
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
 
         [Required]
         [MaxLength(255)]
-        public string? Title { get; set; } 
+        public string? Title { get; set; }
 
-        public string? Description { get; set; } 
+        public string? Description { get; set; }
 
         [Required]
-        public int ReleaseYear { get; set; } 
+        public int ReleaseYear { get; set; }
 
         [Required]
         public int Duration { get; set; }
-
         public string? PosterUrl { get; set; }
 
         [Required]
-        public int GenreId { get; set; } 
+        public int GenreId { get; set; }
         [ForeignKey("GenreId")]
         public Genre? Genre { get; set; }
 
         [Required]
-        public int DirectorId { get; set; } 
+        public int DirectorId { get; set; }
         [ForeignKey("DirectorId")]
         public Director? Director { get; set; }
 
-        public ICollection<MovieActor> MovieActors { get; set; } = new List<MovieActor>(); 
+        public ICollection<MovieActor> MovieActors { get; set; } = new List<MovieActor>();
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; 
+
+        public Statistic? Statistic { get; set; }
+        
+        public ICollection<Review>? Reviews { get; set; }
+        
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
