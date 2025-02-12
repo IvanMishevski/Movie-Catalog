7ca08260d1d9cd962a277baa900b9c8a768a8e8e
Ivan <ivmishevski@gmail.com>
2025-02-12 20:38:34 +0200

    Added StatisticsController

---
+++ Movie Catalog/Controllers/StatisticsAndReviewsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movie_Catalog.Models;
+
+namespace Movie_Catalog.Controllers
+{
+    public class StatisticsAndReviewsController : Controller
+    {
+        private readonly ILogger<StatisticsAndReviewsController> _logger;
+        private readonly MovieCatalogContext _context;
+
+        public StatisticsAndReviewsController(ILogger<StatisticsAndReviewsController> logger, MovieCatalogContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = _context.Movies
+            .Include(m=>m.Statistic)
+            .Include(m=>m.Reviews)
+            .AsQueryable();
+
+            return View("StatisticsAndReviews",movies.ToList());
+        }
+    }
+}
