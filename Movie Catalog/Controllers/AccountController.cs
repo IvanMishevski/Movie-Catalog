ef4a22a74c1dcede5ec2fa74db2d8dc11fe2a706
aylin-s06 <123801066+aylin-s06@users.noreply.github.com>
2025-02-14 11:51:58 +0200

    Added POST: Account/Login Update AccountController.cs

--- Movie Catalog/Controllers/AccountController.cs
+++ Movie Catalog/Controllers/AccountController.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Identity;
-using MovieCatalog.Models;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Movie_Catalog.Models;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace MovieCatalog.Controllers
+namespace Movie_Catalog.Controllers
 {
     public class AccountController : Controller
     {
@@ -30,7 +30,6 @@ namespace MovieCatalog.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Hash the password before saving
                 string hashedPassword = HashPassword(model.Password);
 
                 var user = new User
@@ -42,16 +41,64 @@ namespace MovieCatalog.Controllers
                     CreatedAt = DateTime.UtcNow
                 };
 
-                // Save to database
+                // Записване в базата данни
                 _context.Users.Add(user);
                 await _context.SaveChangesAsync();
 
-                // Redirect after successful registration
+                // Пренасочване след успешна регистрация
                 return RedirectToAction("Index", "Home");
             }
 
             return View(model);
         }
+
+        // GET: Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Find the user by email
+                var user = await _context.Users
+                    .FirstOrDefaultAsync(u => u.Email == model.Email);
+
+                if (user != null)
+                {
+                    // Check the password
+                    if (VerifyPassword(model.Password, user.PasswordHash))
+                    {
+                        // Save user info in session
+                        HttpContext.Session.SetString("Username", user.Username);
+                        HttpContext.Session.SetString("UserEmail", user.Email);
+
+                        // Set success message
+                        TempData["SuccessMessage"] = "Successfully logged in!";
+
+                        // Redirect to the home page
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Invalid password.");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User does not exist.");
+                }
+            }
+
+            return View(model);
+        }
+
+
 
         private string HashPassword(string password)
         {
@@ -65,6 +112,12 @@ namespace MovieCatalog.Controllers
                 }
                 return builder.ToString();
             }
+        }
+
+        private bool VerifyPassword(string enteredPassword, string storedHash)
+        {
+            string hashedPassword = HashPassword(enteredPassword);
+            return hashedPassword == storedHash;
         }
     }
 }
